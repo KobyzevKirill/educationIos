cfd060ab05dcf3da43be58e95f9c6bdb90c492ca
education <education@simbirsoft.com>
2019-01-10 18:02:26 +0300

    TaskOne

---
+++ iOSMobileArchitecture/Classes/Presentation/TaskOneScreen/TaskOneViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TaskOneViewController.swift
+//  iOSMobileArchitecture
+//
+//  Created by user on 10.01.2019.
+//  Copyright © 2019 Andrey Chernyshev. All rights reserved.
+//
+
+import UIKit
+
+class TaskOneViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
